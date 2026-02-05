722f243f07092bb6876cee92cd4662adeeb89e35
connor-kfitz <connorkfitzsimmons@gmail.com>
2026-02-04 20:46:36 -0500

    Updated Secrets Vs Vars

--- app/components/home/Contact.tsx
+++ app/components/home/Contact.tsx
@@ -21,8 +21,8 @@ export default function ContactSection() {
   const [showConfirm, setShowConfirm] = useState(false);
   const [errorState, setErrorState] = useState<ModalErrorState>({ open: false, title: "", message: "" });
 
-  const SERVICE_ID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || "";
-  const TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || "";
+  const SERVICE_ID = process.env.MAILJS_SERVICE_ID || "";
+  const TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID || "";
   const PUBLIC_KEY = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY || "";
 
   const handleSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
